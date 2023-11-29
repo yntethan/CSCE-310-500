3a2bb85d63843506594d2a69b09aa91d151bb175
Michael Tao <mtao369@gmail.com>
2023-11-29 00:06:08 -0600

    set up

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    include_once 'includes/dbh.inc.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $sql = "SELECT * FROM users;";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+
+        if ($resultCheck >= 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo $row['First_Name'] . "<br>";
+            }
+        }
+    ?>
+</body>
+</html>
