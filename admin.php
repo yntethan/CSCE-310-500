24cdf230a19784ae640ac2ec60e73f829aea0e44
wsjoshua02tamu <wsjoshua02@tamu.edu>
2023-12-01 18:42:48 -0600

    adding login system and student view

---
+++ admin.php
@@ -0,0 +1,98 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//displays session data for debugging
+//$_SESSION['test'] = 'Hello, Session!';
+//var_dump($_SESSION);
+
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.php');
+	exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>System Users</h2>
+    <?php include 'includes/displayUsers.inc.php'; ?>
+    <br>
+
+    <h2>Add an admin to the system</h2>
+    <form action="includes/insertAdmin.inc.php" method="POST">
+        <input type="text" name="uin" placeholder="UIN">
+        <br>
+        <input type="text" name="first" placeholder="First Name">
+        <br>
+        <input type="text" name="middle" placeholder="Middle Initial">
+        <br>
+        <input type="text" name="last" placeholder="Last Name">
+        <br>
+        <input type="text" name="username" placeholder="Username">
+        <br>
+        <input type="text" name="password" placeholder="Password">
+        <br>
+        <input type="text" name="email" placeholder="Email">
+        <br>
+        <input type="text" name="discord" placeholder="Discord Name">
+        <br>
+        <button type="submit" name="submit">Add new admin</button>
+    </form>
+    <br>
+
+    <h2>Update information of a user in the system</h2>
+    <p>Enter the fields to be updated (other than UIN, only fill out fields to be updated; can leave fields blank if no change)</p>
+    <form action="includes/updateUser.inc.php" method="POST">
+        <input type="text" name="uin" placeholder="UIN">
+        <br>
+        <input type="text" name="first" placeholder="First Name">
+        <br>
+        <input type="text" name="middle" placeholder="Middle Initial">
+        <br>
+        <input type="text" name="last" placeholder="Last Name">
+        <br>
+        <input type="text" name="username" placeholder="Username">
+        <br>
+        <input type="text" name="password" placeholder="Password">
+        <br>
+        <input type="text" name="type" placeholder="User Type">
+        <br>
+        <input type="text" name="email" placeholder="Email">
+        <br>
+        <input type="text" name="discord" placeholder="Discord Name">
+        <br>
+        <button type="submit" name="submit">Update User</button>
+    </form>
+    <br>
+
+    <h2>Remove a user's access from the system</h2>
+    <form action="includes/removeAccess.inc.php" method="POST">
+        <input type="text" name="uin" placeholder="UIN">
+        <br>
+        <button type="submit" name="submit">Remove access for user</button>
+    </form>
+    <br>
+
+
+
+    <h2>Delete a user from the system</h2>
+    <form action="includes/fullDelete.inc.php" method="POST">
+        <input type="text" name="uin" placeholder="UIN">
+        <br>
+        <button type="submit" name="submit">Delete User</button>
+    </form>
+
+    
+
+
+
+</body>
+</html>
---
+++ includes/authenticate.inc.php
@@ -0,0 +1,54 @@
+<?php
+    session_start();
+    include_once 'dbh.inc.php';
+
+    if ( !isset($_POST['username'], $_POST['password']) ) {
+        // Could not get the data that should have been sent.
+        exit('Please fill both the username and password fields!');
+    }
+
+    if ($stmt = $conn->prepare('SELECT UIN, Passwords, User_Type FROM Users WHERE username = ?')) {
+
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        // Store the result so we can check if the account exists in the database.
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($UIN, $password, $userType);
+            $stmt->fetch();
+            // Account exists, now we verify the password.
+            // Note: remember to use password_hash in your registration file to store the hashed passwords.
+            if ($_POST['password'] === $password) {
+                // Verification success! User has logged-in!
+                // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+                session_regenerate_id();
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION['uin'] = $UIN;
+                $_SESSION['userType'] = $userType;
+
+                //code for handling logins for admins
+                //header('Location: ../admin.php');
+
+                //code for handling logins for different user types
+                if ($_POST['userType'] === 'Admin') {
+                    header('Location: ../admin.php');
+                } elseif ($_POST['userType'] === 'Student') {
+                    header('Location: ../student.php');
+                } else {
+                    // Redirect to a default page if User_Type is neither admin nor student.
+                    header('Location: ../index.php');
+                }
+                exit();
+            } else {
+                // Incorrect password
+                echo 'Incorrect username and/or password!';
+            }
+        } else {
+            // Incorrect username
+            echo 'Incorrect username and/or password!';
+        }
+    
+        $stmt->close();
+    }
